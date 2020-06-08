adf6e3b1b496ed12ddfbfcbcf36b34bb9a68c221
Óscar García <ogbaro@gmail.com>
2020-06-08 17:57:32 +0200

    + Create foundation of the project.

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by iMac Óscar on 08/06/2020.
+//  Copyright © 2020 Óscar García. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
